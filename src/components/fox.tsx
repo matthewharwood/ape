4699f65dff99cdf7dfee6cea94320eeb2562549e
Matthew Harwood <harwood@uber.com>
2020-08-09 00:48:32 -0700

    added live reload

--- src/components/fox.tsx
+++ src/components/fox.tsx
@@ -6,7 +6,7 @@ type PropType = {
 
 const Fox = ({ name }: PropType) => (
   <div className="fox">
-    <h5>{name}1234asdfsaf</h5>
+    <h5>{name}</h5>
     <p>This page is all about {name}.</p>
   </div>
 );
--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,28 @@
+import { join, dirname } from "path";
 import express from "express";
 import { PORT, CURRENT_DOMAIN } from "./constants.mjs";
-
 import render from "preact-render-to-string";
 import Block from "./static/bundle.js";
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
+const liveReloadServer = livereload.createServer();
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+
+liveReloadServer.watch(join(__dirname, "src", "static"));
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+app.use(connectLivereload());
+app.use(express.static("/"));
+app.use("/static", express.static(join(__dirname + "/static")));
 
 app.get("/", (req, res) => {
   let html = render(Block.Fox({ name: "hello" }));
